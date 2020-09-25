08c2ec0713c549cc148fd827122ddb755f3f2f81
Amrinder Rai <amrinderrai94@gmail.com>
2020-09-25 21:29:33 +0100

    Final env components submission
    For thesis 25/09/2020 submission

--- components/board.py
+++ components/board.py
@@ -13,11 +13,10 @@ class Board():
     def place_obstacle(self, coord):
         self.cover_cell(coord, self.OBSTACLE_CELL)
 
-    def off_grid(self, coord): # Where coord is off the grid or not.. 
+    def off_grid(self, coord): 
         return coord[0]<0 or coord[0]>=self.height or coord[1]<0 or coord[1]>=self.width
 
-    def diag_collision(self, prev_pos, new_pos, direction): # TO DO: Test if this works... perhaps better in pcb_envs.py
-        # TO DO: Refactor!! This could be written much better lol :) ... 
+    def diag_collision(self, prev_pos, new_pos, direction): 
         if direction==1 or direction==3:
             adj1 = (new_pos[0],new_pos[1]-1)
             adj2 = (prev_pos[0],prev_pos[1]+1)
--- components/connector.py
+++ components/connector.py
@@ -13,15 +13,14 @@ class Connector():
     def __init__(self, start_coord, idx): 
         self.id = idx 
         self.trail = self.id
-        self.head = self.id + 0.3 # Was 0.1
-        self.goal = self.id + 0.6 # Was 0.2 
+        self.head = self.id + 0.3 
+        self.goal = self.id + 0.6
         self.connection = [start_coord] 
 
     def get_head(self): 
         return self.connection[-1]
     
     def move(self, direction):
-        # Environment should always give available actions as 0 to 7 :) Where should the error handling go??? 
         self.connection.append(self._step(direction))
 
     def pop_head(self):
